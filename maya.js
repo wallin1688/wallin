dc07b2d052f342f77f65ffa1a0b457a0c5472639
wangsu111 <707199781@qq.com>
2019-11-07 11:36:09 +0800

    123

--- maya.js
+++ maya.js
@@ -1,8 +1,23 @@
+$('.words').mouseover(function(){
+  $(this).css({
+    "background":"#5285a6"
+  }).siblings().css({
+    "background":"none"
+  })
+})
 $('.file').click(function(){
     if(!$(".list1").is(":visible")){
         $(".list1").show(); 
+        $(this).css({
+          "background":"#5285a6"
+        }).siblings().css({
+          "background":"none"
+        })
        }else{ 
         $(".list1").hide(); 
+        $(this).css({
+          "background":"none"
+        })
       }
     $(".list1").siblings().hide();
     $(".nav1 ul").show();
@@ -11,8 +26,16 @@ $('.file').click(function(){
 $('.edit').click(function(){
     if(!$(".list2").is(":visible")){
         $(".list2").show(); 
+        $(this).css({
+          "background":"#5285a6"
+        }).siblings().css({
+          "background":"none"
+        })
        }else{ 
         $(".list2").hide(); 
+        $(this).css({
+          "background":"none"
+        })
       }
     $(".list2").siblings().hide();
     $(".nav1 ul").show();
@@ -21,8 +44,16 @@ $('.edit').click(function(){
 $('.establish').click(function(){
   if(!$(".list3").is(":visible")){
       $(".list3").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list3").hide(); 
+      $(".list3").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list3").siblings().hide();
   $(".nav1 ul").show();
@@ -31,8 +62,16 @@ $('.establish').click(function(){
 $('.select').click(function(){
   if(!$(".list4").is(":visible")){
       $(".list4").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list4").hide(); 
+      $(".list4").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list4").siblings().hide();
   $(".nav1 ul").show();
@@ -40,9 +79,17 @@ $('.select').click(function(){
 
 $('.amend').click(function(){
   if(!$(".list5").is(":visible")){
-      $(".list5").show(); 
+      $(".list5").show();
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      }) 
      }else{ 
-      $(".list5").hide(); 
+      $(".list5").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list5").siblings().hide();
   $(".nav1 ul").show();
@@ -51,8 +98,16 @@ $('.amend').click(function(){
 $('.display').click(function(){
   if(!$(".list6").is(":visible")){
       $(".list6").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list6").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list6").siblings().hide();
   $(".nav1 ul").show();
@@ -61,8 +116,16 @@ $('.display').click(function(){
 $('.wicket').click(function(){
   if(!$(".list7").is(":visible")){
       $(".list7").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list7").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list7").siblings().hide();
   $(".nav1 ul").show();
@@ -71,8 +134,16 @@ $('.wicket').click(function(){
 $('.gridding').click(function(){
   if(!$(".list8").is(":visible")){
       $(".list8").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list8").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list8").siblings().hide();
   $(".nav1 ul").show();
@@ -81,8 +152,16 @@ $('.gridding').click(function(){
 $('.editGrid').click(function(){
   if(!$(".list9").is(":visible")){
       $(".list9").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list9").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list9").siblings().hide();
   $(".nav1 ul").show();
@@ -91,8 +170,16 @@ $('.editGrid').click(function(){
 $('.gridTool').click(function(){
   if(!$(".list10").is(":visible")){
       $(".list10").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list10").hide(); 
+      $(".list10").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list10").siblings().hide();
   $(".nav1 ul").show();
@@ -101,18 +188,33 @@ $('.gridTool').click(function(){
 $('.gridShow').click(function(){
   if(!$(".list11").is(":visible")){
       $(".list11").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list11").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list11").siblings().hide();
-  $(".nav1 ul").show();
 })
 
 $('.curve').click(function(){
   if(!$(".list12").is(":visible")){
       $(".list12").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list12").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list12").siblings().hide();
   $(".nav1 ul").show();
@@ -121,8 +223,16 @@ $('.curve').click(function(){
 $('.hookFace').click(function(){
   if(!$(".list13").is(":visible")){
       $(".list13").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list13").hide(); 
+      $(".list13").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list13").siblings().hide();
   $(".nav1 ul").show();
@@ -131,8 +241,16 @@ $('.hookFace').click(function(){
 $('.variant').click(function(){
   if(!$(".list14").is(":visible")){
       $(".list14").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list14").hide(); 
+      $(".list14").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list14").siblings().hide();
   $(".nav1 ul").show();
@@ -141,8 +259,16 @@ $('.variant').click(function(){
 $('.UV').click(function(){
   if(!$(".list15").is(":visible")){
       $(".list15").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list15").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list15").siblings().hide();
   $(".nav1 ul").show();
@@ -151,8 +277,16 @@ $('.UV').click(function(){
 $('.create').click(function(){
   if(!$(".list16").is(":visible")){
       $(".list16").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
-      $(".list16").hide(); 
+      $(".list16").hide();
+      $(this).css({
+        "background":"none"
+      }) 
     }
   $(".list16").siblings().hide();
   $(".nav1 ul").show();
@@ -160,19 +294,36 @@ $('.create').click(function(){
 
 $('.cache').click(function(){
   if(!$(".list17").is(":visible")){
-      $(".list17").show(); 
+      $(".list17").show();
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      }) 
      }else{ 
       $(".list17").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list17").siblings().hide();
   $(".nav1 ul").show();
+
 })
 
 $('.substance').click(function(){
   if(!$(".list18").is(":visible")){
-      $(".list18").show(); 
+      $(".list18").show();
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      }) 
      }else{ 
       $(".list18").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list18").siblings().hide();
   $(".nav1 ul").show();
@@ -181,8 +332,16 @@ $('.substance').click(function(){
 $('.arnold').click(function(){
   if(!$(".list19").is(":visible")){
       $(".list19").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list19").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list19").siblings().hide();
   $(".nav1 ul").show();
@@ -192,8 +351,16 @@ $('.arnold').click(function(){
 $('.help').click(function(){
   if(!$(".list20").is(":visible")){
       $(".list20").show(); 
+      $(this).css({
+        "background":"#5285a6"
+      }).siblings().css({
+        "background":"none"
+      })
      }else{ 
       $(".list20").hide(); 
+      $(this).css({
+        "background":"none"
+      })
     }
   $(".list20").siblings().hide();
   $(".nav1 ul").show();
