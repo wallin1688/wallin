b2acaa28edc9c973c50a39cc5299d766b0bee835
wangsu111 <707199781@qq.com>
2019-11-15 17:56:03 +0800

    2019-11-15

--- maya.js
+++ maya.js
@@ -1,372 +1,89 @@
-$('.words').mouseover(function(){
-  $(this).css({
-    "background":"#5285a6"
-  }).siblings().css({
-    "background":"none"
-  })
-})
-$('.file').click(function(){
-    if(!$(".list1").is(":visible")){
-        $(".list1").show(); 
+// $('.words').mouseover(function(){
+//   $(this).css({
+//     "background":"#5285a6"
+//   }).siblings().css({
+//     "background":"none"
+//   })
+//   if($(this).show()){
+     
+
+//   }
+// })
+
+// $('.file').mouseover(function(){
+//     $(".list1").show(); 
+//     $(this).css({
+//       "background":"#5285a6"
+//     }).siblings().css({
+//       "background":"none"
+//     })
+
+// $(".list1").siblings().hide();
+// $(".nav1 ul").show();
+// })
+// $(".file").mouseout(function(){
+//   $(".list1").hide(); 
+//   $(this).css({
+//     "background":"none"
+//   })
+// })
+
+$('.words').click(function(event){
+  
+    if(!$(this).children(".Wlist").is(":visible")){
+        $(this).children(".Wlist").show(); 
         $(this).css({
           "background":"#5285a6"
         }).siblings().css({
           "background":"none"
         })
        }else{ 
-        $(".list1").hide(); 
+        $(this).children(".Wlist").hide(); 
         $(this).css({
           "background":"none"
         })
       }
-    $(".list1").siblings().hide();
-    $(".nav1 ul").show();
+      event.stopPropagation();
+    $(this).siblings().children(".Wlist").hide();
+    $(this).parents('.Wnav').siblings().find('.Wlist').hide();
+    $(this).parents('.Wnav').siblings().find('.words').css({
+      "background":"none"
+    })
 })
 
-$('.edit').click(function(){
-    if(!$(".list2").is(":visible")){
-        $(".list2").show(); 
-        $(this).css({
-          "background":"#5285a6"
-        }).siblings().css({
-          "background":"none"
-        })
-       }else{ 
-        $(".list2").hide(); 
-        $(this).css({
-          "background":"none"
-        })
-      }
-    $(".list2").siblings().hide();
-    $(".nav1 ul").show();
+// 二级页面
+$('.rightWord').mouseover(function(){
+  $(this).children('div').show();
+  $(this).css('background','#5285a6');
 })
-
-$('.establish').click(function(){
-  if(!$(".list3").is(":visible")){
-      $(".list3").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list3").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list3").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.select').click(function(){
-  if(!$(".list4").is(":visible")){
-      $(".list4").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list4").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list4").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.amend').click(function(){
-  if(!$(".list5").is(":visible")){
-      $(".list5").show();
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      }) 
-     }else{ 
-      $(".list5").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list5").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.display').click(function(){
-  if(!$(".list6").is(":visible")){
-      $(".list6").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list6").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list6").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.wicket').click(function(){
-  if(!$(".list7").is(":visible")){
-      $(".list7").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list7").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list7").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.gridding').click(function(){
-  if(!$(".list8").is(":visible")){
-      $(".list8").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list8").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list8").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.editGrid').click(function(){
-  if(!$(".list9").is(":visible")){
-      $(".list9").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list9").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list9").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.gridTool').click(function(){
-  if(!$(".list10").is(":visible")){
-      $(".list10").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list10").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list10").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.gridShow').click(function(){
-  if(!$(".list11").is(":visible")){
-      $(".list11").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list11").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list11").siblings().hide();
-})
-
-$('.curve').click(function(){
-  if(!$(".list12").is(":visible")){
-      $(".list12").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list12").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list12").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.hookFace').click(function(){
-  if(!$(".list13").is(":visible")){
-      $(".list13").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list13").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list13").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.variant').click(function(){
-  if(!$(".list14").is(":visible")){
-      $(".list14").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list14").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list14").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.UV').click(function(){
-  if(!$(".list15").is(":visible")){
-      $(".list15").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list15").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list15").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.create').click(function(){
-  if(!$(".list16").is(":visible")){
-      $(".list16").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list16").hide();
-      $(this).css({
-        "background":"none"
-      }) 
-    }
-  $(".list16").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.cache').click(function(){
-  if(!$(".list17").is(":visible")){
-      $(".list17").show();
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      }) 
-     }else{ 
-      $(".list17").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list17").siblings().hide();
-  $(".nav1 ul").show();
-
-})
-
-$('.substance').click(function(){
-  if(!$(".list18").is(":visible")){
-      $(".list18").show();
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      }) 
-     }else{ 
-      $(".list18").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list18").siblings().hide();
-  $(".nav1 ul").show();
-})
-
-$('.arnold').click(function(){
-  if(!$(".list19").is(":visible")){
-      $(".list19").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list19").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list19").siblings().hide();
-  $(".nav1 ul").show();
+$('.rightWord').mouseout(function(){
+  $(this).children().eq(2).hide();
+  $(this).css('background','none');
 })
 
 
-$('.help').click(function(){
-  if(!$(".list20").is(":visible")){
-      $(".list20").show(); 
-      $(this).css({
-        "background":"#5285a6"
-      }).siblings().css({
-        "background":"none"
-      })
-     }else{ 
-      $(".list20").hide(); 
-      $(this).css({
-        "background":"none"
-      })
-    }
-  $(".list20").siblings().hide();
-  $(".nav1 ul").show();
+// 菜单栏切换的点击事件
+$('.nav2-1').click(function(){
+  if(!$('.nav2-list').is(":visible")){
+    $('.nav2-list').show();
+   }else{ 
+    $('.nav2-list').hide();
+  }
+})
+
+$('.tab').mouseover(function(){
+  $(this).css('background',"#5285a6");
+  $(this).siblings(".tab").css('background',"none")
+})
+ 
+// 点击切换选项卡
+$('.tab').click(function(){
+  $('.nav2-1-1').find('span').eq($(this).index()).show().siblings().hide();
+  $('.nav1').find('.Wnav').eq($(this).index()+1).show().siblings('.changeAdd').hide();
 })
 
 
-
-
-
+$(document).click(function(){
+  $(".Wlist").hide();
+  });
+  
